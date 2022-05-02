ae7a7fb34751a9e8c43bd4355246d1289405a7f5
JesminNancy <jesminnancy@hotmail.com>
2022-05-02 12:43:39 +0600

    Basic Routing with Controller

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -1,36 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SiteController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [SiteController::class, 'Home']);
 
-Route::get('/hello', function(){
-    return "Hello World";
-});
+Route::get('/about', [SiteController::class, 'About']);
 
-Route::get('/name', function(){
-    return "I am Nancy";
-});
-
-Route::get('/home', function(){
-    return view('homepage');
-});
-Route::get('/about', function(){
-    return view('aboutpage');
-});
-Route::get('/contact', function(){
-    return view('contactpage');
-});
+Route::get('/contact', [SiteController::class, 'Contact']);
