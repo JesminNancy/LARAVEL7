2f9e1fbd7b81869f5c9f57eeaeab26d3255ce165
JesminNancy <jesminnancy@hotmail.com>
2022-05-09 22:50:59 +0600

    Basic Routing Parameter Controller To View

--- blog/app/Http/Controllers/ParameterController.php
+++ blog/app/Http/Controllers/ParameterController.php
@@ -12,9 +12,18 @@ class ParameterController extends Controller
     
     }
     
+    // Basic Routing With Multiple Parameter
+    // function Name($firstName,$middleName,$lastName,$age){
+    
+    //     return "First Name: " .$firstName . "<br>Middle Name: ".$middleName . "<br> Last Name: ". $lastName. "<br> My Age: ".$age;
+    
+    // }
+    
+     // Basic Routing Parameter Controller To View
+     
     function Name($firstName,$middleName,$lastName,$age){
     
-        return "First Name: " .$firstName . "<br>Middle Name: ".$middleName . "<br> Last Name: ". $lastName. "<br> My Age: ".$age;
+     return view('DemoPage', ['firstname'=>$firstName, 'middlename'=>$middleName, 'lastname'=>$lastName, 'age'=>$age]);
     
     }
 }
---
+++ blog/resources/views/DemoPage.blade.php
@@ -0,0 +1,4 @@
+<h1>First Name:{{$firstname}}</h1>
+<h1>Middle Name:{{$middlename}}</h1>
+<h1>Last Name:{{$lastname}}</h1>
+<h1>My Age:{{$age}}</h1>
