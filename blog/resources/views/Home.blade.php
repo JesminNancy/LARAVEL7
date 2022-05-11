e4389c084ec5283c3db1e6c79f14dcc87d2b8469
JesminNancy <jesminnancy@hotmail.com>
2022-05-11 18:54:30 +0600

    Including subview Blade View

--- blog/resources/views/Home.blade.php
+++ blog/resources/views/Home.blade.php
@@ -1,14 +1,10 @@
-@php
-    
-   $num1= 30;
-   $num2= 50;
-   
-   $result = $num1 + $num2 ;
-   
-   echo $result . "<br>";
-   
-   echo time();
-@endphp
+@include('subview.Menu')
+<br>
+<br><br><br><br><br><br><br><br><br><br><br>
+<h1>This is Home Page</h1>
+<br><br><br><br><br><br><br>
+@include('subview.Footer')
+
 
 
 
---
+++ blog/resources/views/subview/Footer.blade.php
@@ -0,0 +1,3 @@
+<div style="width: 100%;height:100px;background-color:brown">
+  <h1>This is footer</h1>
+</div>
