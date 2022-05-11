8b6770a0aea3168a9d37394efea80a978a443981
JesminNancy <jesminnancy@hotmail.com>
2022-05-11 13:00:52 +0600

    For Loop Blade View

--- blog/resources/views/Home.blade.php
+++ blog/resources/views/Home.blade.php
@@ -2,3 +2,14 @@
 {{ $newsHeadlinekey }}
 
 {!!$newsHeadlinekey!!}
+
+@for($i=0; $i<=100; $i++)
+
+  <button>{{ $i }}</button>
+@endfor
+<br><br><br><br>
+@for($i=0; $i<100; $i++)
+
+<img style="width:100px;height:50px;" src="https://media.istockphoto.com/photos/hands-protecting-globe-of-green-tree-on-tropical-nature-summer-and-picture-id1206797959?s=612x612">
+  
+@endfor
