11af7780b7f4ecb6e25b3e9c8411b79c522c3f60
JesminNancy <jesminnancy@hotmail.com>
2022-05-12 11:00:14 +0600

    Data Passing to subview

--- blog/resources/views/Home.blade.php
+++ blog/resources/views/Home.blade.php
@@ -1,10 +1,7 @@
 @include('subview.Menu')
-<br>
-<br><br><br><br><br><br><br><br><br><br><br>
-<h1>This is Home Page</h1>
-<br><br><br><br><br><br><br>
-@include('subview.Footer')
-
-
+<br><br><br><br><br><br><br><br><br><br><br><br>
+<h1>This is Body Content</h1>
+<br><br><br><br><br><br><br><br><br>
+@include('subview.Footer', ['footertextKey'=>"All Right Reserved By Donalt Trump"])
 
 
